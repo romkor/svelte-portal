9da3535d974e4fb141198c59004a9f695d4f1459
Roman Rodych <romko.ua@gmail.com>
2019-11-05 22:45:01 +0200

    Add Portal.svelte

---
+++ README.md
@@ -0,0 +1,3 @@
+Svelte component for rendering outside the DOM of parent component
+
+Idea borrowed from here: https://github.com/sveltejs/svelte/issues/3088#issuecomment-505785516
---
+++ src/Portal.svelte
@@ -0,0 +1,21 @@
+<script>
+  import { onMount, onDestroy } from "svelte";
+
+  let portal;
+  let componentInstance;
+
+  onMount(() => {
+    portal = document.createElement("div");
+    portal.className = "svelte-portal";
+    document.body.appendChild(portal);
+    portal.appendChild(componentInstance);
+  });
+
+  onDestroy(() => {
+    document.body.removeChild(portal);
+  });
+</script>
+
+<div bind:this={componentInstance}>
+  <slot />
+</div>
