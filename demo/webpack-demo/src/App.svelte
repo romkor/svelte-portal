bb8372487a638d8745626f6766bcc1fa4a4afa7f
Roman Rodych <romko.ua@gmail.com>
2020-09-14 21:40:01 +0300

    Reexport Portal component as default export Bump release-it Add rollup and webpack demo projects

---
+++ demo/webpack-demo/src/App.svelte
@@ -0,0 +1,15 @@
+<script>
+  import Portal from 'svelte-portal'
+
+  export let name
+</script>
+
+<style>
+  h1 {
+    color: purple;
+  }
+</style>
+
+<h1>Hello {name}!</h1>
+
+<Portal>Hello from Portal</Portal>
