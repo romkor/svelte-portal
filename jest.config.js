107847a493eeaf30abeefb2bee6602f18ca19a0b
johannchopin <johannchopin@pm.me>
2020-10-17 18:35:47 +0200

    🔧 Config jest, babel

---
+++ jest.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  transform: {
+    '^.+\\.js?$': 'babel-jest',
+    '^.+\\.svelte?$': 'jest-transform-svelte'
+  },
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1'
+  },
+  moduleFileExtensions: ['js', 'svelte'],
+  bail: false,
+  verbose: true,
+  transformIgnorePatterns: ['<rootDir>/node_modules/'],
+  setupFilesAfterEnv: ['@testing-library/jest-dom/extend-expect']
+}
