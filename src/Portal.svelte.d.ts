66defc7771d12a3c4995337334691b4977715c90
m93a <m93a.cz@gmail.com>
2022-06-02 17:25:30 +0200

    fix formatting of Portal.svelte.d.ts

--- src/Portal.svelte.d.ts
+++ src/Portal.svelte.d.ts
@@ -5,27 +5,33 @@ import { SvelteComponentTyped } from "svelte";
  * @param {HTMLElement} el
  * @param {HTMLElement|string} target DOM Element or CSS Selector
  */
-export declare function portal(el: HTMLElement, target?: HTMLElement | string): {
-    update: (newTarget: HTMLElement | string) => Promise<void>;
-    destroy: () => void;
+export declare function portal(
+  el: HTMLElement,
+  target?: HTMLElement | string
+): {
+  update: (newTarget: HTMLElement | string) => Promise<void>;
+  destroy: () => void;
 };
 declare const __propDef: {
-    props: {
-        /**
-             * DOM Element or CSS Selector
-             * @type { HTMLElement|string}
-             */ target?: string | HTMLElement | undefined;
-    };
-    events: {
-        [evt: string]: CustomEvent<any>;
-    };
-    slots: {
-        default: {};
-    };
+  props: {
+    /**
+     * DOM Element or CSS Selector
+     * @type { HTMLElement|string}
+     */ target?: string | HTMLElement | undefined;
+  };
+  events: {
+    [evt: string]: CustomEvent<any>;
+  };
+  slots: {
+    default: {};
+  };
 };
 export declare type PortalProps = typeof __propDef.props;
 export declare type PortalEvents = typeof __propDef.events;
 export declare type PortalSlots = typeof __propDef.slots;
-export default class Portal extends SvelteComponentTyped<PortalProps, PortalEvents, PortalSlots> {
-}
+export default class Portal extends SvelteComponentTyped<
+  PortalProps,
+  PortalEvents,
+  PortalSlots
+> {}
 export {};
