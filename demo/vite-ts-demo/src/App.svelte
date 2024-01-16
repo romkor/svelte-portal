68d208fe8d2a7567f7ae63dacadb17f92ece5742
Roman Rodych <romko.ua@gmail.com>
2024-01-16 22:03:39 +0200

    chore: Change path for exports.svelte in package.json
    Add demo example with vite

---
+++ demo/vite-ts-demo/src/App.svelte
@@ -0,0 +1,66 @@
+<script lang="ts">
+  import Portal, { portal } from "svelte-portal";
+  import Avatar from "./libs/Avatar.svelte";
+  import Cube from "./libs/Cube.svelte";
+
+  type PortalColor = "orange" | "blue";
+
+  let visible = true;
+  let color: PortalColor = "orange";
+
+  function toggle() {
+    visible = !visible;
+  }
+  function swap() {
+    color = color === "orange" ? "blue" : "orange";
+  }
+  let avatarHostEl = document.getElementById("avatar") as HTMLElement;
+</script>
+
+<main>
+  <h1>Svelte-Portal</h1>
+  <p>
+    <Portal target={avatarHostEl}>
+      <Avatar />
+    </Portal>
+
+    <button on:click={toggle}>{visible ? "hide" : "show"}</button>
+    <button on:click={swap}>Swap cubes</button>
+    {#if visible}
+      <Portal>Portal content injected into the body</Portal>
+      <div use:portal hidden>
+        This is also injected into the body, but without a wrapper element
+      </div>
+    {/if}
+  </p>
+  <!-- 
+    When `display: contents` is supported, the browser will ignore the wrapper div.
+    That why the flex-direction different between the red and blue portals.
+  -->
+  <div use:portal={`.portal--${color}`} style="display: contents" hidden>
+    <Cube />
+    <Cube />
+  </div>
+</main>
+
+<style>
+  main {
+    text-align: center;
+    padding: 1em;
+    max-width: 240px;
+    margin: 0 auto;
+  }
+
+  h1 {
+    color: #ff3e00;
+    text-transform: uppercase;
+    font-size: 4em;
+    font-weight: 100;
+  }
+
+  @media (min-width: 640px) {
+    main {
+      max-width: none;
+    }
+  }
+</style>
